8150adf2fb81e79dad8852980f95d990e70c0b2c
rakuspHoshi <ryuto.hoshi@rakus-partners.co.jp>
2022-02-04 15:04:45 +0900

    Itemの作成

---
+++ src/main/java/com/example/domain/Item.java
@@ -0,0 +1,28 @@
+package com.example.domain;
+
+public class Item {
+	
+	private String name;
+	private int price;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return "Item [name=" + name + ", price=" + price + "]";
+	}
+	
+	
+
+}
